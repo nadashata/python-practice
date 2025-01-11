01324f1a21ea837fab415a01b61b12063f6f2b56
Nada Mohamed Magdy Mohamed <45911177+nadashata@users.noreply.github.com>
2025-01-11 22:30:46 +0200

    Update rock_paper_scissors.py
    remove 2 redundant if statements, by using a list for images

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -25,28 +25,13 @@ scissors = '''
 ---.__(___)
 '''
 # 0 rock, 1 paper , 2 scissors
+choice_image=[rock, paper, scissors]
 import random
 computer_choice = random.randint(0,2)
-computer_icon = ""
-if computer_choice == 0:
-    computer_icon=rock
-elif computer_choice == 1:
-    computer_icon=paper
-else:
-    computer_icon=scissors
 user_choice = int(input("Welcome to the game\nPlease choose, 0 For rock, 1 For Paper or 2 for scissors: "))
 if user_choice == 0 or user_choice == 1 or user_choice ==2 :
-    user_icon = ""
-    if user_choice == 0:
-        user_icon=rock
-    elif user_choice == 1:
-        user_icon=paper
-    elif user_choice == 2:
-        user_icon=scissors
-    print("Your Choice:")
-    print(user_icon)
-    print("Computer Choice:")
-    print(computer_icon)
+    print(f"Your Choice:\n{choice_image[user_choice]}")
+    print(f"Computer Choice:\n{choice_image[computer_choice]}")
     if computer_choice == user_choice:
        print("Its Draw")
     else:
