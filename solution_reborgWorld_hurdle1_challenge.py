8238895282223d39fd0dd7da5efc93c20002c139
Nada Mohamed Magdy Mohamed <45911177+nadashata@users.noreply.github.com>
2025-01-13 00:21:15 +0200

    Create solution_reborgWorld_hurdle1_challenge.py

---
+++ solution_reborgWorld_hurdle1_challenge.py
@@ -0,0 +1,20 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# Reborg World Hurdle 1 
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for i in range(0,6):
+    move()
+    jump()
